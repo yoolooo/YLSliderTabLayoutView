7d3e0a8d99a77d2fd1417045a32b1e3828214264
yoolooo <864275697@qq.com>
2018-08-23 15:36:10 +0800

    第一次提交

---
+++ Example/YLSliderTabLayout/ContentViewController.h
@@ -0,0 +1,15 @@
+//
+//  ContentViewController.h
+//  YLSliderTabberView
+//
+//  Created by yan on 2018/8/23.
+//  Copyright © 2018年 yanlong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ContentViewController : UIViewController
+
+@property (nonatomic, strong) NSString *titleString;
+
+@end
---
+++ Example/YLSliderTabLayout/ContentViewController.m
@@ -0,0 +1,41 @@
+//
+//  ContentViewController.m
+//  YLSliderTabberView
+//
+//  Created by yan on 2018/8/23.
+//  Copyright © 2018年 yanlong. All rights reserved.
+//
+
+#import "ContentViewController.h"
+
+@interface ContentViewController ()
+
+@property (nonatomic, weak) UILabel *label;
+
+@end
+
+@implementation ContentViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+    
+    UILabel *label = [[UILabel alloc] initWithFrame:CGRectZero];
+    label.textColor = [UIColor redColor];
+    label.font = [UIFont systemFontOfSize:20];
+    label.textAlignment = NSTextAlignmentCenter;
+    self.label = label;
+    [self.view addSubview:label];
+    
+    NSLog(@"viewDidLoad");
+    
+}
+
+- (void)setTitleString:(NSString *)titleString{
+    self.label.text = titleString;
+    [self.label sizeToFit];
+    self.label.center = self.view.center;
+}
+
+
+@end
